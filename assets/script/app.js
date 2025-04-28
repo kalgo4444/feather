7de29c17efca2e1cf9df9585df255b9bd6ffb789
kalgo4444 <abuxyziabd@gmail.com>
2025-04-28 14:48:30 +0500

    add js

---
+++ assets/script/app.js
@@ -0,0 +1,84 @@
+{
+    // 1 - masala 
+    // function kvadratToTwo(num){
+    //     return num * 2
+    // }
+    // console.log(kvadratToTwo(5));
+}
+{
+    // 2 - masala 
+    // function helloPlusName(fname){
+    //     return `Salom, ${fname}!`
+    // }
+    // console.log(helloPlusName("User"));
+}
+{
+    // 3 - masala 
+    // function numberPlus(num) {
+    //     if (num > 0) {
+    //         return "musbat"
+    //     } else if (0 > num) {
+    //         return "manfiy"
+    //     } else {
+    //         return num
+    //     }
+    // }
+    // console.log(numberPlus(0));
+}
+{
+    // 4 - masaala 
+    // function oddEnv(num){
+    //     return num % 2 == 0 ? "juft" : "toq"
+    // }
+    // console.log(oddEnv(5));
+}
+{
+    // 5 - masala 
+    // function maxReturn(num1,num2){
+    //     return num1 < num2 ? num2 : num1
+
+    //     return Math.max(num1, num2)
+    // }
+    // console.log(maxReturn(3, 5));
+}
+{
+    // 6 - masala 
+    // function numberInt(num) {
+    //     for (let i = 1; i <= num; i++) {
+    //         console.log(i);
+    //     }
+    // }
+
+    // numberInt(3)
+}
+{
+    // 7 - masala
+    // function arrSum(arr) {
+    //     let sum = 0
+    //     for (let i = 1; i <= arr.length; i++) {
+    //         sum += i
+    //     }
+    //     return sum
+    // }
+    // console.log(arrSum([1, 2, 3]));
+}
+
+{
+    // 8 - masala !
+}
+{
+    
+    // 9 - masala !
+
+}
+{
+    // 10 - masala 
+    // function factorial(num) {
+    //     let sum = 1;
+    //     for (let i = 1; i <= num; i++) {
+    //         sum *= i
+    //     }
+    //     return sum
+    // }
+    // console.log(factorial(5));
+}
